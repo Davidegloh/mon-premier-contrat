8d4d7d627e1092075ad7e9786e386448fab95031
David Egloh <davidegloh.oclock@gmail.com>
2021-09-10 10:03:23 +0200

    ajout de l'interface

--- bank.sol
+++ bank.sol
@@ -1,5 +1,20 @@
 pragma solidity 0.7.5; 
 
-contract Bank {
+import "./Ownable.sol";
+import "./Destroyable.sol";
+
+// ----------------- L'INTERFACE----------------
+// Toujours positionner à la racine d'un contrat. In order to interact with the Gov contract, our contract needs to know the "function definition" of the Gov function, the input and the output of the gov contract. 
+// We also need to know where the Gov contract is localized. For all of this, we need an interface. 
+
+interface GovernmentInterface {
+    // On spécifie les fonctions avec lesquelles on veut intéragir. On  a besoin  de savoir comment la fonction est appelée,quels sont ses arguments et ce qu'il retourne(s'il retourne quelque chose).
+        function addTransaction(address _from, address _to, uint _amount) payable external; 
+
+}
+//---------------------------------------------
+
+
+contract Bank is Ownable, Destroyable { // Ce contract hérite de Ownable.sol et Destroyable.sol {
 
 }
