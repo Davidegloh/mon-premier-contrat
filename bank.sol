8d2fcf9b07b5702f045d983a945aa27b6dc97c30
David Egloh <davidegloh.oclock@gmail.com>
2021-09-10 10:13:05 +0200

    Exemple de strings, arrays, mappings et structs

--- bank.sol
+++ bank.sol
@@ -19,7 +19,25 @@ interface GovernmentInterface {
     //memory - temporary storage used in function execution
     //call data - save arguments/input to our functions
     
+    //strings, arrays, mappings, structs
+    
+    
+    // Mapping
+    mapping(address => uint) balance; // Is it useful to have a variable balance to our contract so that our contract can query for its own balance
 
+// Instance de l'interface 
+   //On créé une instance du contrat Gov dans notre Bank contract. On spécifie l'adresse du contrat externe Gov afin d'intéragir avec le Gov contrat.
+    GovernmentInterface GovernmentInstance = GovernmentInterface(0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8);
+
+  
+  // Event 
+    event depositDone(uint amount, address indexed depositedTo); // Event : C'est une façon de log  des events et de la data during a function execution. En arguments s'affichera
+    // le montant déposé et l'addresse recevant les fonds. 
+    
+       event withdrawDone(uint amount, address sender); // Event : C'est une façon de log  des events et de la data during a function execution. En arguments s'affichera
+    // le montant déposé et l'addresse d'envoi et l'adresse recevant les fonds. 
+
+    event transfertDone(uint amount, address sender, address recipient); 
 
 contract Bank is Ownable, Destroyable { // Ce contract hérite de Ownable.sol et Destroyable.sol {
 
