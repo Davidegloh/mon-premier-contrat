55aaddd3a0363c8bbfb781374185a9c3a84a12bb
David Egloh <davidegloh.oclock@gmail.com>
2021-09-13 09:47:41 +0200

    création du struct gouvernement

--- Government.sol
+++ Government.sol
@@ -1,14 +1,11 @@
 pragma solidity 0.7.5; 
 
 contract Government {
-      // On créer un struct de transaction. Chaque transaction se composera d'une adresse from, to, d'un montant et d'un Id de transaction
-    struct Transaction {
-        address from;
-        address to;
-        uint amount;
-        uint txId;
-        
-      
-    }
+   struct Transaction{
+       address from;
+       address to;
+       uint amount;
+       uint txId; 
+   }   
     
 }
