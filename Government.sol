f4fe1206e55fe3d5179b9cd19cbe1f1506a7e09a
David Egloh <davidegloh.oclock@gmail.com>
2021-09-13 10:03:57 +0200

    ajout de la fonction add transaction

--- Government.sol
+++ Government.sol
@@ -1,11 +1,25 @@
 pragma solidity 0.7.5; 
 
+// On crée un struct de transaction. Chaque transaction se composera d'une adresse from, to, d'un montant et d'un Id de transaction
 contract Government {
    struct Transaction{
        address from;
        address to;
        uint amount;
        uint txId; 
-   }   
+   }  
+
+   //on crée un array de transaction pour stocker les transactions.
+   Transaction[] transactionLog;
+
+
+   
+   function addTransaction(address _from, address _to, uint amount) payable external {// on créé une fonction qui ajoute des transactions.
+    // on créer une variable qui s'appelle "_transaction" qu'on stock en mémoire et qui contient en argument: adresse from, adresse to, le montant et l'id de la transaction que l'on retient 
+      Transaction memory _transaction  = Transaction (_from, _to, amount, transactionLog.length); 
+      // on pousse (push) la variable _transaction dans le tableau (Array) "TransactionLog"
+      TransactionLog.push(_transaction);
+   }
+
     
 }
